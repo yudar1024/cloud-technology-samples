59efe890663a7e884762d016d526f94acfca9925
yudar1024 <chen.rojer@gmail.com>
2019-05-21 15:01:52 +0800

    move location

--- src/main/java/com/mycompany/myapp/config/KeyCloakSecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/KeyCloakSecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.mycompany.myapp.security;
+package com.mycompany.myapp.config;
 
+import com.mycompany.myapp.security.AuthoritiesConstants;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
