506ca95a574998b169c4b26b5bfc3aca10144865
yudar1024 <chen.rojer@gmail.com>
2019-05-27 15:22:06 +0800

    get current username

--- src/main/java/com/mycompany/myapp/web/rest/examples/RolebasedExample.java
+++ src/main/java/com/mycompany/myapp/web/rest/examples/RolebasedExample.java
@@ -5,12 +5,16 @@ import io.swagger.annotations.ApiKeyAuthDefinition;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.SecurityDefinition;
 import io.swagger.annotations.SwaggerDefinition;
+import org.keycloak.KeycloakSecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api")
@@ -22,7 +26,12 @@ public class RolebasedExample {
 
     @GetMapping(value = "/admin")
     @Secured("ROLE_ADMIN")
-    public String admin() {
+    public String admin(HttpServletRequest request) {
+        KeycloakSecurityContext keycloakSecurityContext = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
+        String userName = keycloakSecurityContext.getToken().getPreferredUsername();
+
+        log.info("user name = "+userName);
+        log.info("user in security context  = "+SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         log.info("admin invoked");
         return "Admin";
     }
