33cbb72b566dfe2c6735259cbd4b1475eb13782f
yudar1024 <rojer.chen@foxmail.com>
2019-09-24 09:59:51 +0800

    Initial version of usermgmt generated by JHipster-6.3.1

---
+++ usermgmt/src/main/java/com/mycompany/usermgmt/config/Constants.java
@@ -0,0 +1,12 @@
+package com.mycompany.usermgmt.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
