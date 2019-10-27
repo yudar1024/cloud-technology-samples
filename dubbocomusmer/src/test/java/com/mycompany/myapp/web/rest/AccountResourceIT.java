72105cf47c32a5b292bab3bbf8ff9b6fe717d960
yudar1024 <rojer.chen@foxmail.com>
2019-10-27 21:46:20 +0800

    fix error

--- dubbocomusmer/src/test/java/com/mycompany/myapp/web/rest/AccountResourceIT.java
+++ dubbocomusmer/src/test/java/com/mycompany/myapp/web/rest/AccountResourceIT.java
@@ -9,6 +9,7 @@ import com.mycompany.myapp.web.rest.errors.ExceptionTranslator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
