4ad64834c490e36c5f7600fde307951bb54b3c61
yudar1024 <rojer.chen@foxmail.com>
2019-12-26 14:00:05 +0800

    Generated with JHipster 6.6.0 and

--- sprcloudaliuser/src/main/java/com/mycompany/ApplicationWebXml.java
+++ sprcloudaliuser/src/main/java/com/mycompany/ApplicationWebXml.java
@@ -1,6 +1,7 @@
 package com.mycompany;
 
-import com.mycompany.config.DefaultProfileUtil;
+import io.github.jhipster.config.DefaultProfileUtil;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
