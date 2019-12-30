c239fc2373de610ec0012b0426596ff6e88c7910
yudar1024 <rojer.chen@foxmail.com>
2019-12-30 12:04:41 +0800

    Initial version of sprcloudalipay generated by JHipster-6.6.0

---
+++ sprcloudalipay/src/main/java/com/mycompany/pay/config/Constants.java
@@ -0,0 +1,12 @@
+package com.mycompany.pay.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
