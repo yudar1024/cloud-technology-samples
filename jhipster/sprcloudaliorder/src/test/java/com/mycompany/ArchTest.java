4111a3a8f950071eccb8c156e18ba6bf8772c8b6
yudar1024 <rojer.chen@foxmail.com>
2020-03-19 11:44:39 +0800

    Generated with JHipster 6.8.0

--- jhipster/sprcloudaliorder/src/test/java/com/mycompany/ArchTest.java
+++ jhipster/sprcloudaliorder/src/test/java/com/mycompany/ArchTest.java
@@ -18,9 +18,9 @@ class ArchTest {
 
         noClasses()
             .that()
-                .resideInAnyPackage("..service..")
+                .resideInAnyPackage("com.mycompany.service..")
             .or()
-                .resideInAnyPackage("..repository..")
+                .resideInAnyPackage("com.mycompany.repository..")
             .should().dependOnClassesThat()
                 .resideInAnyPackage("..com.mycompany.web..")
         .because("Services and repositories should not depend on web layer")
