b32575c74a7a9bda4b0adf52ec78fd28bb66adea
yudar1024 <rojer.chen@foxmail.com>
2020-05-08 17:51:14 +0800

    update

--- alicloud/alicloudapp/src/main/java/com/mycompany/alicloudapp/seata/DatabaseConfiguration.java
+++ alicloud/alicloudapp/src/main/java/com/mycompany/alicloudapp/seata/DatabaseConfiguration.java
@@ -22,6 +22,8 @@ import com.alibaba.druid.pool.DruidDataSource;
 
 import io.seata.rm.datasource.DataSourceProxy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,44 +42,16 @@ public class DatabaseConfiguration {
 		this.applicationContext = applicationContext;
 	}
 
-	@Bean(initMethod = "init", destroyMethod = "close")
-	public DruidDataSource storageDataSource() throws SQLException {
-
-		Environment environment = applicationContext.getEnvironment();
-
-		String ip = environment.getProperty("mysql.server.ip");
-		String port = environment.getProperty("mysql.server.port");
-		String dbName = environment.getProperty("mysql.db.name");
-
-		String userName = environment.getProperty("mysql.user.name");
-		String password = environment.getProperty("mysql.user.password");
-
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DruidDataSource druidDataSource() throws SQLException {
 		DruidDataSource druidDataSource = new DruidDataSource();
-		druidDataSource.setUrl(
-				"jdbc:mysql://" + ip + ":" + port + "/" + dbName + "?serverTimezone=UTC");
-		druidDataSource.setUsername(userName);
-		druidDataSource.setPassword(password);
-		druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		druidDataSource.setInitialSize(0);
-		druidDataSource.setMaxActive(180);
-		druidDataSource.setMaxWait(60000);
-		druidDataSource.setMinIdle(0);
-		druidDataSource.setValidationQuery("Select 'x' from DUAL");
-		druidDataSource.setTestOnBorrow(false);
-		druidDataSource.setTestOnReturn(false);
-		druidDataSource.setTestWhileIdle(true);
-		druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
-		druidDataSource.setMinEvictableIdleTimeMillis(25200000);
-		druidDataSource.setRemoveAbandoned(true);
-		druidDataSource.setRemoveAbandonedTimeout(1800);
-		druidDataSource.setLogAbandoned(true);
-		druidDataSource.setFilters("mergeStat");
 		return druidDataSource;
 	}
 
 	@Bean
-	public DataSourceProxy dataSourceProxy(DruidDataSource druidDataSource) {
-		return new DataSourceProxy(druidDataSource);
+	public DataSourceProxy dataSourceProxy(DruidDataSource druid) {
+		return new DataSourceProxy(druid);
 	}
 
 	@Bean
