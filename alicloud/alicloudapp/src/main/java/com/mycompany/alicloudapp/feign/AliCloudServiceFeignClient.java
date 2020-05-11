bbd0bbc17f476c1c8926147832b41da55e31f795
yudar1024 <rojer.chen@foxmail.com>
2020-05-11 17:59:26 +0800

    add comment

--- alicloud/alicloudapp/src/main/java/com/mycompany/alicloudapp/feign/AliCloudServiceFeignClient.java
+++ alicloud/alicloudapp/src/main/java/com/mycompany/alicloudapp/feign/AliCloudServiceFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "alicloudservice", fallback = AliCloudServiceFeignClient.ServiceFallBack.class,configuration = AliCloudServiceFeignClient.AlicloudServiceAppFeignConfiguration.class )
 //@FeignClient(value = "alicloudservice")
 public interface AliCloudServiceFeignClient {
-
+//   这里会新建一个sentinel资源， name = GET:http://alicloudservice/api/bb
     @GetMapping(value = "/api/bb")
     public String aa(@RequestParam(value = "a") int a, @RequestParam(value = "b") int b);
 
