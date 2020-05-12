1feb0d77d2f61a8e663144bc1c7782379e53cdba
yudar1024 <rojer.chen@foxmail.com>
2020-05-12 11:40:33 +0800

    Initial version of alicloudgateway generated by JHipster-6.8.0

---
+++ alicloud/alicloudgateway/src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -0,0 +1,10 @@
+package com.mycompany.myapp.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+}
---
+++ alicloud/alicloudgateway/src/main/java/com/mycompany/myapp/config/WebConfigurer.java
@@ -0,0 +1,67 @@
+package com.mycompany.myapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.jhipster.config.JHipsterProperties;
+import io.github.jhipster.web.filter.reactive.CachingHttpHeadersFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.reactive.ResourceHandlerRegistrationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
+import org.springframework.web.server.WebExceptionHandler;
+import org.zalando.problem.spring.webflux.advice.ProblemExceptionHandler;
+import org.zalando.problem.spring.webflux.advice.ProblemHandling;
+
+/**
+ * Configuration of web application with Servlet 3.0 APIs.
+ */
+@Configuration
+public class WebConfigurer implements WebFluxConfigurer {
+
+    private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
+
+    private final JHipsterProperties jHipsterProperties;
+
+    public WebConfigurer(JHipsterProperties jHipsterProperties) {
+        this.jHipsterProperties = jHipsterProperties;
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = jHipsterProperties.getCors();
+        if (config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty()) {
+            log.debug("Registering CORS filter");
+            source.registerCorsConfiguration("/api/**", config);
+            source.registerCorsConfiguration("/management/**", config);
+            source.registerCorsConfiguration("/v2/api-docs", config);
+            source.registerCorsConfiguration("/*/api/**", config);
+            source.registerCorsConfiguration("/services/*/api/**", config);
+            source.registerCorsConfiguration("/*/management/**", config);
+        }
+        return new CorsWebFilter(source);
+    }
+
+    // TODO: remove when this is supported in spring-data / spring-boot
+    @Override
+    public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
+        configurer.addCustomResolver(new ReactiveSortHandlerMethodArgumentResolver(),
+            new ReactivePageableHandlerMethodArgumentResolver());
+    }
+
+    @Bean
+    @Order(-2) // The handler must have precedence over WebFluxResponseStatusExceptionHandler and Spring Boot's ErrorWebExceptionHandler
+    public WebExceptionHandler problemExceptionHandler(ObjectMapper mapper, ProblemHandling problemHandling) {
+        return new ProblemExceptionHandler(mapper, problemHandling);
+    }
+
+
+}
