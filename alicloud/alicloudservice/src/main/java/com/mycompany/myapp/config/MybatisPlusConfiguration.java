4229c23a4fe1f840302ca0e029b368c3209b112e
yudar1024 <rojer.chen@foxmail.com>
2020-06-03 15:25:47 +0800

    add seata configuration

--- alicloud/alicloudservice/src/main/java/com/mycompany/myapp/config/MybatisPlusConfiguration.java
+++ alicloud/alicloudservice/src/main/java/com/mycompany/myapp/config/MybatisPlusConfiguration.java
@@ -1,5 +1,7 @@
 package com.mycompany.myapp.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +15,17 @@ public class MybatisPlusConfiguration {
     @Bean
     public PerformanceMonitorInterceptor performanceMonitorInterceptor(){
         return new PerformanceMonitorInterceptor();
+    }
+    /**
+     * MP 自带分页插件
+     *
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setCountSqlParser(new JsqlParserCountOptimize(true));
+
+        return page;
     }
 }
