83e00d977588da7fa541ab883485a6bd6b85e3f8
yudar1024 <rojer.chen@foxmail.com>
2020-09-05 14:23:50 +0800

    for debug

--- bigdata/spark/src/main/scala/org/examples/spark/rdd/file/LocalFileWordCount.scala
+++ bigdata/spark/src/main/scala/org/examples/spark/rdd/file/LocalFileWordCount.scala
@@ -8,7 +8,9 @@ object LocalFileWordCount {
     val conf = new SparkConf().setAppName("word-count").setMaster("local[*]")
     val context = new SparkContext(conf)
     val rdd = context.textFile(args(0))
-    val words = rdd.flatMap(_.split(" "))
+    val words = rdd.flatMap(line => {
+      line.split(" ")
+    })
     val value:RDD[(String,Int)] = words.map((_, 1))
     val reduceRdd = value.reduceByKey(_ + _)
     val sortRdd = reduceRdd.sortBy(_._2, false)
