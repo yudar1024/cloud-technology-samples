dd46601a4d206829c376e59cd561f3b7f2e9e61e
yudar1024 <rojer.chen@foxmail.com>
2020-09-05 11:27:49 +0800

    add spark basic local file example

---
+++ bigdata/spark/src/main/scala/org/examples/spark/rdd/file/LocalFileWordCount.scala
@@ -0,0 +1,18 @@
+package org.examples.spark.rdd.file
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object LocalFileWordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("word-count").setMaster("local[*]")
+    val context = new SparkContext(conf)
+    val rdd = context.textFile(args(0))
+    val words = rdd.flatMap(_.split(" "))
+    val value:RDD[(String,Int)] = words.map((_, 1))
+    val reduceRdd = value.reduceByKey(_ + _)
+    val sortRdd = reduceRdd.sortBy(_._2, false)
+    sortRdd.saveAsTextFile("D:\\devtools\\testdata\\count")
+
+  }
+}
